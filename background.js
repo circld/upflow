c72d3a4e550d81094d38043b8a9ecd46e942a80d
circld <circld1@gmail.com>
2017-05-11 21:37:53 -0500

    add first pass at timer behavior; many edge cases...

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+// TODO: `redirected` implementation is wrong; how to get `keep-growing.html` to
+// report state
 // hardcoded blacklist
 var blacklist = [
   "manga",
@@ -17,22 +19,71 @@ function isBlacklistedFactory(blacklist) {
   }
 }
 
+var upTimeSet = 10;
+var downTimeSet = 5;
+// var upTimeSet = 40 * 60;
+// var downTimeSet = 20 * 60;
+var totalTime = upTimeSet + downTimeSet;
+var downTime = downTimeSet;
+var periodSeconds = new Array(totalTime).fill(0);
 var isBlacklisted = isBlacklistedFactory(blacklist);
+var redirected = false;
 
-// TODO: send blacklisted URL along to provide link on blocked page
+function sumArray(arr) {
+  var total = 0;
+  for (var i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+function isDownTime() {
+  return downTime <= 0;
+}
+
+// TODO: visibility: handle multiple windows (!= lastfocusedwindow)
+// TODO: bug: if on blacklisted site & open new window, downTime stays same forever
+function sendActivetTabMessage(message) {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, message);
+  });
+}
+
 function validateUrl(request, sender, sendResponse) {
   var url = request.url;
-  var responseAction = isBlacklisted(url) ? 'redirect' : 'noop';
-  sendResponse({
+  var responseAction = isBlacklisted(url) && isDownTime() ? 'redirect' : 'noop';
+  redirected = responseAction === 'redirect' ? true : false;
+  sendActivetTabMessage({
     action: responseAction,
     currenturl: url,
     redirectUrl: chrome.extension.getURL('keep-growing.html')
   });
 }
 
+// check active tab URL every second
+function checkUrl() {
+  console.log(downTime);
+  if ( redirected ) {
+    periodSeconds.push(0);
+    downTime += periodSeconds.shift();
+    return;
+  }
+  sendActivetTabMessage({action: "urlCheckAsk", time: downTime, period: periodSeconds});
+}
+setInterval(checkUrl, 1000);
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.action === 'urlLoad') {
+    if ( request.action === "urlLoad" ) {
+      validateUrl(request, sender, sendResponse);
+    } else if ( request.action === "urlCheckReply" ) {
+      let newest = isBlacklisted(request.url) ? 1 : 0;
+      let oldest = periodSeconds.shift();
+      periodSeconds.push(newest);
+
+      downTime -= downTime > 0 ? newest : 0;
+      downTime += downTime < downTimeSet ? oldest : 0;
       validateUrl(request, sender, sendResponse);
     }
   }
--- content.js
+++ content.js
@@ -1,11 +1,21 @@
-// TODO: convert everything into camelCase
-// TODO: refactor to intercept before even loading the page:
+// TODO: refactor to intercept before even loading the page?
 // http://stackoverflow.com/questions/13571110/chrome-extension-how-to-redirect-to-a-custom-html-page-in-response-to-specific
-chrome.runtime.sendMessage(
-  {action: 'urlLoad', url: window.location.href},
-  function(response) {
-    if ( response.action === 'redirect' ) {
-      window.location.href = response.redirectUrl;
+
+// on new page load
+chrome.runtime.sendMessage({action: "urlLoad", url: window.location.href});
+
+// listener for request to check url
+chrome.runtime.onMessage.addListener(
+
+  function(request, sender, sendResponse) {
+    if ( request.action === "redirect" ) {
+      window.location.href = request.redirectUrl;
+    } else if ( request.action === "urlCheckAsk" ) {
+      console.log(request);
+      chrome.runtime.sendMessage(
+          {action: "urlCheckReply", url: window.location.href}
+      );
     }
   }
+
 );
